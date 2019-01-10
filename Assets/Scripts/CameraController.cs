0fba0d064f9f6418f95f5745dd8e1bb03cc9d35a
Grant Okamoto <gsokamoto@cpp.edu>
2019-01-09 19:06:53 -0800

    Day 2 Progress
    - Added a generic turret
    - Added turret projectiles
    - Added tower generation on nodes
    - Added camera movement for player
    - Next feature: shop

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+
+    private bool doMovement = true;
+
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+
+	void Update ()
+    {
+        //allows user to lock screen with esc
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            doMovement = !doMovement;
+        }
+
+        if(!doMovement)
+        {
+            return;
+        }
+		if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        // allow scroll zooming
+        Vector3 pos = transform.position;
+        pos.y -= scroll * 750 * scrollSpeed * Time.deltaTime;
+
+        // setting limits on screen movement
+        pos.x = Mathf.Clamp(pos.x, -60, 12);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -75, 5);
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 offset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't build there - TODO: Display on screen");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + offset, transform.rotation);
+    }
+
+    void OnMouseEnter()
+    {
+        //GetComponent<Renderer>().material.color = hoverColor; could do it like this instead, but less optimized
+        rend.material.color = hoverColor;
+
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
